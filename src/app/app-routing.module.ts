d568fe705285e229114801ff8996f1d677470ea7
nagasawa <nagasawa944@gmail.com>
2022-06-29 16:49:22 +0900

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsComponent } from './forms/forms.component';
 import { StandaloneComponent } from './standalone/standalone.component';
 import { TraditionComponent } from './tradition/tradition.component';
 
